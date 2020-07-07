5bb4efe9b7bc666fecd05d520de004e0fbbff1f6
priitmaek <priit09@gmail.com>
2020-07-08 01:51:18 +0300

    Lisatud tooted

---
+++ src/tooted/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Tooted
+
+
+admin.site.register(Tooted)
+# Register your models here.
---
+++ src/tooted/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TootedConfig(AppConfig):
+    name = 'tooted'
